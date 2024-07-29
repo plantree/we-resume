4b36e8e8cfc7dedd97bb83cda90bcdcb8a0a719a
pengyuanwang <wpy1174555847@outlook.com>
2024-07-29 17:39:56 +0800

    [2024-7-29] just a test for husky

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
-    '*.{s,jsx,ts,tsx,json,css,scss,md}': [
+    '*.{js,jsx,ts,tsx,json,md}': [
         'eslint --report-unused-disable-directives --max-warnings 0',
-        'prettier --write \"src/**/*.{js,jsx,ts,tsx,json,css,scss,md}\ && stylelint './**/*.{css,scss,less}' --fix"'
+        'prettier --write \"src/**/*.{js,jsx,ts,tsx,json,md}\"',
+    ],
+    '*.{css,scss}': [
+        'stylelint --fix'
     ]
 };
